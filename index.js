e3687c56171437ac0ada1c611433f5ceab267ddb
Dustin Huntsberger <dhunts258@gmail.com>
2022-04-13 16:38:24 -0400

    added most of the markdown code

--- index.js
+++ index.js
@@ -1,13 +1,99 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the project's title?"
+    },
+    {
+        type: "input",
+        name: "repo",
+        message: "Enter your exact Github repository name."
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What email should people use to reach you with questions?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your Github username?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter a description of your project."
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Which license are you using?",
+        choices: ["MIT", "Apache", "None"]
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            throw new Error('File failed to save: ' + err);
+        }
+
+        return "Save successful.";
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions);
+}
+
+const installPrompt = readmeData => {
+    if (!readmeData.install) {
+        readmeData.install = [];
+    }
+
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "text",
+            message: "Please enter the next step."
+        },
+        {
+            type: "confirm",
+            name: "confirm",
+            message: "Enter another step?"
+        }
+    ])
+    .then(installStep => {
+        readmeData.install.push(installStep);
+        if (installStep.confirm) {
+            return installPrompt(readmeData);
+        } else {
+            console.log(readmeData);
+            return readmeData;
+        }
+    })
+}
 
 // Function call to initialize app
-init();
+init()
+    .then(generalQuestions => {
+        return installPrompt(generalQuestions);
+    })
+    .then(fullResponse => {
+        console.log(fullResponse);
+        return generateMarkdown(fullResponse);
+    })
+    .then(readmeMarkdown => {
+        return writeToFile('./dist/README.md', readmeMarkdown);
+    })
+    .catch(err => {
+        console.log(err);
+    });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicense(license) {
+   
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -8,12 +10,51 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(data) {
+    if (data.license == "None") {
+        return ""
+    }
+
+    return `
+## License
+![GitHub](https://img.shields.io/github/license/${data.github}/${data.repo})
+  `
+}
+
+const installMarkDown = installSteps => {
+    return installSteps
+    .map( (step, index) => {
+        return `${index + 1}. ${step.text}
+`
+    })
+    .join('')
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+
+  ## Installation Instructions
+  ${installMarkDown(data.install)}
+
+  ## Usage
+
+  ${renderLicenseSection(data)}
+
+  ## Contributing
+
+  ## Tests
+
+  ## Questions?
+  If you have any more questions for me, I can be reached from one of the links below. Thank you!
+  - [Email Me!](mailto:${data.email})
+  - [Visit me on Github!](https://github.com/${data.github})
 `;
 }
 
